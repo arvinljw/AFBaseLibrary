e6e18a5668a0ec0eed566f7facff926f8c8faac2
刘金伟 <jin-wei.liu@utsoft.cn>
2017-05-11 00:50:51 +0800

    构建项目
    从新开始构建这个项目，第一步搭建框架，拟定初步计划

---
+++ sample/src/main/java/net/arvin/sample/uis/activities/MainActivity.java
@@ -0,0 +1,22 @@
+package net.arvin.sample.uis.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import net.arvin.sample.R;
+
+/**
+ * Created by arvinljw on 17/5/10 12:51
+ * Function：
+ * Desc：
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ sample/src/main/java/net/arvin/sample/uis/activities/SampleActivity.java
@@ -0,0 +1,15 @@
+package net.arvin.sample.uis.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.arvin.sample.R;
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
